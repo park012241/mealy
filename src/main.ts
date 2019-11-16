a41e48c23086f806bf0a37150a8969386460e7e9
Joosung Park <admin@slowmotion.dev>
2019-11-16 23:29:22 +0900

    add: Swagger API Docs

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import {NestFactory} from '@nestjs/core';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 import {start} from 'elastic-apm-node';
 import {AppModule} from './app.module';
 
@@ -10,6 +11,17 @@ if (process.env.ES_APM) {
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV !== 'production') {
+    const options = new DocumentBuilder()
+      .setTitle('Mealy')
+      .setDescription('API of Mealy')
+      .setVersion(process.env.npm_package_version || '1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(process.env.PORT || 3000, process.env.HOST);
 };
 

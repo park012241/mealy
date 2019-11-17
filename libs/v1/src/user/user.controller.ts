9ce3cdce4172e714f0c4dbc241fc70a78970e7b5
Joosung Park <admin@slowmotion.dev>
2019-11-17 23:54:57 +0900

    Add: Change Password

--- libs/v1/src/user/user.controller.ts
+++ libs/v1/src/user/user.controller.ts
@@ -1,5 +1,6 @@
-import {Body, Controller, Post, ValidationPipe} from '@nestjs/common';
+import {Body, Controller, Patch, Post, ValidationPipe} from '@nestjs/common';
 import {ApiOperation, ApiUseTags} from '@nestjs/swagger';
+import {ChangePasswordDto} from './dto/change-password.dto';
 import {UserDto} from './dto/user.dto';
 import {UserService} from './user.service';
 
@@ -17,5 +18,13 @@ export class UserController {
   })
   public async addUser(@Body(new ValidationPipe()) body: UserDto) {
     return this.userService.newUser(body);
+  }
+
+  @Patch()
+  @ApiOperation({
+    title: 'Update Password',
+  })
+  public async changePassword(@Body(new ValidationPipe()) body: ChangePasswordDto) {
+    return this.userService.changePassword(body);
   }
 }
--- libs/v1/src/user/user.service.ts
+++ libs/v1/src/user/user.service.ts
@@ -1,13 +1,19 @@
 import {DatabaseService} from '@app/database';
-import {Injectable} from '@nestjs/common';
+import {ForbiddenException, Inject, Injectable, InternalServerErrorException, NotFoundException} from '@nestjs/common';
+import {JsonWebTokenError} from 'jsonwebtoken';
 import {Collection, GridFSBucket} from 'mongodb';
+import {TokenKind, TokenPayload} from '../auth/interfaces/token-payload.interface';
+import {JsonWebToken} from '../auth/json-web-token';
 import {Constants} from '../constants';
 import {User} from '../types';
+import {ChangePasswordDto} from './dto/change-password.dto';
 import {UserDto} from './dto/user.dto';
 import {PassCodeInterface} from './interfaces/pass-code.interface';
 
 @Injectable()
 export class UserService {
+  @Inject(JsonWebToken)
+  private readonly jwt: JsonWebToken;
   private readonly passCodes: Collection<PassCodeInterface>;
   private readonly users: Collection<User>;
   private readonly userProfileImages: GridFSBucket;
@@ -42,5 +48,36 @@ export class UserService {
     }, await pc.next()));
 
     await pc.destroy();
+  }
+
+  public async changePassword({password, token}: ChangePasswordDto): Promise<void> {
+    let data: TokenPayload;
+    try {
+      data = this.jwt.verify(token, {
+        subject: TokenKind.access,
+      });
+    } catch (e) {
+      if (e instanceof JsonWebTokenError) {
+        throw new ForbiddenException(e.message.toString());
+      } else {
+        throw new InternalServerErrorException(e.message.toString());
+      }
+    }
+
+    if (!await this.users.find({
+      username: data.id,
+    }).hasNext()) {
+      throw new NotFoundException('Couldn\'t find User');
+    }
+
+    await this.users.updateOne({
+      username: {
+        $eq: data.id,
+      },
+    }, {
+      $set: {
+        password: Buffer.from(password),
+      },
+    });
   }
 }
